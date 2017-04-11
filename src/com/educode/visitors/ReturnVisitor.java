9fe5ea75a66786f5f5c4937b3764e7c0b47d7c8e
BullDuckling <tbuhla@gmail.com>
2017-04-11 11:49:49 +0200

    RV new nodes added

--- src/com/educode/visitors/ReturnVisitor.java
+++ src/com/educode/visitors/ReturnVisitor.java
@@ -6,10 +6,7 @@ import com.educode.nodes.base.Node;
 import com.educode.nodes.expression.AdditionExpression;
 import com.educode.nodes.expression.MultiplicationExpression;
 import com.educode.nodes.expression.logic.*;
-import com.educode.nodes.literal.BoolLiteralNode;
-import com.educode.nodes.literal.IdentifierLiteralNode;
-import com.educode.nodes.literal.NumberLiteralNode;
-import com.educode.nodes.literal.StringLiteralNode;
+import com.educode.nodes.literal.*;
 import com.educode.nodes.method.MethodDeclarationNode;
 import com.educode.nodes.method.MethodInvocationNode;
 import com.educode.nodes.method.ParameterNode;
@@ -22,6 +19,7 @@ import com.educode.nodes.statement.conditional.RepeatWhileNode;
 import com.educode.nodes.ungrouped.BlockNode;
 import com.educode.nodes.ungrouped.ObjectInstantiationNode;
 import com.educode.nodes.ungrouped.ProgramNode;
+import com.educode.nodes.ungrouped.TypeCastNode;
 import com.educode.symboltable.Symbol;
 import com.educode.symboltable.SymbolTableHandler;
 import com.educode.types.ArithmeticOperator;
@@ -181,6 +179,12 @@ public class ReturnVisitor extends VisitorBase
     }
 
     @Override
+    public Object visit(CoordinatesLiteralNode node)
+    {
+        return null;
+    }
+
+    @Override
     public Object visit(IdentifierLiteralNode node) { return null; }
 
     @Override
@@ -203,4 +207,7 @@ public class ReturnVisitor extends VisitorBase
 
     @Override
     public Object visit(NegateNode node) { return null; }
+
+    @Override
+    public Object visit(TypeCastNode node) { return null; }
 }
