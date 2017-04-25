8c186204277cf6bf9688b6a4dcd807745e7b03f7
SimonLinnebjerg <simon.linnebjerg@hotmail.com>
2017-04-25 10:42:27 +0200

    fuck intellij

--- src/main/java/com/educode/helper/InterfaceConverter.java
+++ src/main/java/com/educode/helper/InterfaceConverter.java
@@ -3,8 +3,6 @@ package com.educode.helper;
 import com.educode.runtime.types.*;
 import com.educode.symboltable.SymbolTable;
 import com.educode.types.Type;
-import scala.xml.PrettyPrinter;
-import sun.awt.X11.XTranslateCoordinates;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
